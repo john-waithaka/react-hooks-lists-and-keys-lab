75694759da37511e1c8cb02c9bae8a2b429ff3b3
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-05-27 15:43:48 +0300

    lists and keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+//Warui add code to map array
+  const technologiesList = technologies.map((tech) => (
+    <span key={tech}>{tech}</span>
+  ));
+
   return (
+
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologiesList}</div>
     </div>
   );
 }
+
 
 export default ProjectItem;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,28 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+  
+       {/* Warui Add code to map the array and include working props*/}
+        
+       const projectItems = projects.map((project) => (
+        <ProjectItem
+        key={project.id}
+        name={project.name}
+        about={project.about}
+        technologies={project.technologies}
+      />
+      ))
+
+        {/* what should we see */}
+
+        return (
+          <div id="projects">
+            <h2>My Projects</h2>
+            <div id="project-list">{projectItems}</div>
+          </div>
+        );
+
+
 }
 
 export default ProjectList;
